3e3d38ee8e857ec63c404a9e18e97d8616de6e06
boluo <boluokk@hotmail.com>
2023-09-09 21:59:45 +0800

    2023.09.09 21:59 fix bug

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ update_source_fallback = update_source
 -- apk level 限制
 is_apk_old = function() return getApkVerInt() < 0 end
 apk_old_warning = "怎么还有人用" .. getApkVerInt()
-release_date = "09.09 21:54"
+release_date = "09.09 21:59"
 release_content = '添加红装暂停、购买体力+旗帜; 移除刷标签类型判定'
 -- 获取workPath
 root_path = getWorkPath() .. '/'
