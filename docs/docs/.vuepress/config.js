646efd8619302a58b0fe38ce9dd4ccc813d2b145
boluo <boluokk@hotmail.com>
2023-09-11 23:26:19 +0800

    2023.09.11 23:26 更新文档

--- docs/docs/.vuepress/config.js
+++ docs/docs/.vuepress/config.js
@@ -7,17 +7,17 @@ module.exports = {
         sidebar: "auto",
         nav: [
             {
-                text: '加入QQ群',
+                text: '加入QQ群🐧',
                 link: 'https://qm.qq.com/cgi-bin/qm/qr?k=o6MW-K-Ws6A2-S_WhHEro1JggzREWPzt&jump_from=webapi&authKey=PRSHw0kOjKtnqEVwyXCRHnKKwAWhzXWD/y486deoyZ/AWyNfGLwHIEwjb8gf9yoX'
             },
             {
-                text: '下载链接',
+                text: '下载链接📩',
                 items: [
                     {text: '下载链接(密码6666)', link: 'https://wwos.lanzouj.com/ioVOc117nsqh'}
                 ]
             },
             {
-                text: '功能执行步骤',
+                text: '软件功能详细介绍👍',
                 link: '/zh/sequence'
             }
         ],
