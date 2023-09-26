af7c4e3bf669fb28537401c297f2cae09b021306
boluo <boluokk@hotmail.com>
2023-09-26 20:05:13 +0800

    2023.09.26 20:05 替换cover图片

--- main.lua
+++ main.lua
@@ -5,7 +5,7 @@ update_source_fallback = update_source
 -- apk level 限制
 is_apk_old = function() return getApkVerInt() < 0 end
 apk_old_warning = "怎么还有人用" .. getApkVerInt()
-release_date = "09.26 19:55"
+release_date = "09.26 20:04"
 release_content = '修复已知问题, 添加部分功能'
 -- 获取workPath
 root_path = getWorkPath() .. '/'
